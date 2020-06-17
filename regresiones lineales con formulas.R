a60862280840f4a7aacc0b8153501e7151c55d04
Madeline Salguero <61562317+madeeeliiiin@users.noreply.github.com>
2020-06-17 16:23:11 -0600

    Update regresiones lineales con formulas.R

--- regresiones lineales con formulas.R
+++ regresiones lineales con formulas.R
@@ -1,4 +1,10 @@
-#Regresiones lineales casos de covid
-
 library(dplyr)
 library(ggplot2)
+
+casos<-read.csv("infcovid.csv", sep = "")
+
+modelo1<-lm(casos$Dia~casos$Casos)
+summary(modelo1)
+modelo1$coefficients
+casos$cont_aprox<-casos$Casos[1] * exp(casos$Dia *modelo1$coefficients[2])
+
