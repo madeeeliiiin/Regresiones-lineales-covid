a82e635f366bb07594835f64e1874b104d2fdc62
Madeline Salguero <61562317+madeeeliiiin@users.noreply.github.com>
2020-07-02 22:43:02 -0600

    Update regresiones lineales.r

--- regresiones lineales.r
+++ regresiones lineales.r
@@ -5,7 +5,7 @@ casos<-read.csv("casos.csv", sep = ";")
 names(casos)[1] <- "Dia"
 names(casos)[2] <- "Casos"
 
-########################## Regresiones lineales manual########################## 
+########################## Regresiones lineales manual Muertes########################## 
 #creamos la matriz A
 matriz_A<-matrix(data = 1,nrow = nrow(casos),1)
 dia<-(casos$Dia)-1
@@ -56,15 +56,78 @@ casos$casos_aprox<- exp(contagio_dia_1)*exp(casos$Dia *tasa)
   #Calculo de casos aproximados con exp(contagio2_dia_1)*exp(tasa2*dia + c2*((dia)^2)))
   casos$casos2_aprox<- exp(contagio2_dia_1)*exp(tasa2*dia + c2*((dia)^2))
   
+  ########################## Regresiones lineales fallecimientos manual########################## 
+  #creamos la matriz A
+  
+  fallecidos<-casos$Fallecidos
+  #Es necesario no tener los 0 en el vector de fallecidos
+  i<-1
+  while(i<length(fallecidos)){
+    if(fallecidos[i]>0){
+      fallecidos<-fallecidos[i]
+    }
+    i<-i+1}
+  fallecidos<-casos$Fallecidos[(i-1):nrow(casos)]
+  
+  Fallecimientos_A<-matrix(data = 1,nrow = length(fallecidos),1)
+  Fallecimientos_A<-cbind(Fallecimientos_A,dia)
+  
+  # Matriz con Ln de los casos
+  Fallecimientos_Ln<-log(fallecidos)
+  
+  # Multiplicacion de (Inversa de A^t*A)*(matriz_A^T *Matriz_ln)
+  Fallecimientos_Ln_K<-solve(t(Fallecimientos_A) %*% Fallecimientos_A)  %*% (t(Fallecimientos_A) %*% Fallecimientos_Ln)
+  
+  #Calculo de muertes aproximados con exp(poblacion inicial)  * EXP(dia *tasa)
+  casos$Fallecidos_aprox[(i-1):nrow(casos)]<- exp(Fallecimientos_Ln_K[1])*exp(casos$Dia *Fallecimientos_Ln_K[2])
+
+  
+  ########################## Regresiones cuadraticas para fallecidos manual########################## 
+  #creamos la matriz 2A
+  Fallecimientos_2A<-matrix(data = 1,nrow = length(fallecidos),1)
+  Fallecimientos_2A<-cbind(Fallecimientos_2A,dia)
+  Fallecimientos_2A<-cbind(Fallecimientos_2A,dia_2)
+  
+  # Multiplicacion de (Inversa de 2A^t*A)*(matriz_2A^T *Matriz_ln)
+  Fallecimientos_2Ln_K<-(solve(t(Fallecimientos_2A) %*% Fallecimientos_2A))%*% ((t(Fallecimientos_2A) %*% Fallecimientos_Ln))
+  
+  #Tasa de crecimiento
+  Fallecimientos_tasa2<-Fallecimientos_2Ln_K[2]
+  Fallecimientos2_dia_1<-Fallecimientos_2Ln_K[1]
+  Fallecimiento_c2<-Fallecimientos_2Ln_K[3]
+  
+  
+  #Calculo de casos aproximados con exp(contagio2_dia_1)*exp(tasa2*dia + c2*((dia)^2)))
+  casos$Fallecimientos2_aprox[(i-1):nrow(casos)]<- exp(Fallecimientos2_dia_1)*exp(Fallecimientos_tasa2*dia + Fallecimiento_c2*((dia)^2))
+
+  
+  
   #Graficar los casos y las regresiones lineales
   ggplot(data = casos,mapping = aes(x = Dia,y = Casos))+
     geom_point()+
     labs(title = 'casos coronavirus')+
     stat_smooth(mapping = aes(x=Dia,y=casos2_aprox,colour="regresion cuadratica"))+
     stat_smooth(mapping = aes(x = Dia,y = casos_aprox,colour="regresion lineal"))
+  
+  #Grafica de los fallecimientos
+  ggplot(data = casos,mapping = aes(x = Dia,y = Fallecidos))+
+    geom_point()+
+    stat_smooth(mapping = aes(x = Dia,y = Fallecidos_aprox,colour="Fallecidos aproximados"))+
+    stat_smooth(mapping = aes(x = Dia,y = Fallecimientos2_aprox,colour="Fallecidos aproximados cuadraticos"))
  
-  #Graficos de la escala logaritmica
+  #Graficos de la escala logaritmica de los casos
   ggplot(data = casos,mapping = aes(x = Dia,y = log(Casos),colour="Casos"))+
     geom_point()+
     labs(title = 'escala logaritmica')+
-    stat_smooth(mapping = aes(x=Dia,y=log(casos_aprox),colour="casos aprox")) 
+    geom_smooth(mapping = aes(x = Dia,y=log(casos_aprox),colour="casos aprox"),method = "lm")
+  
+  #Grafica de la escala logaritmica de muertes 
+  ggplot(data = casos,mapping = aes(x = Dia,y = log(Fallecidos),colour="Fallecidos"))+
+    geom_point()+
+    labs(title = 'escala logaritmica fallecidos')+
+    geom_smooth(mapping = aes(x = Dia,y=log(Fallecidos_aprox),colour="Fallecidos aprox"),method = "lm")
+  
+  #Grafica de casos actuales y muertos
+  ggplot(data = casos,mapping = aes(x = Dia,y = Fallecidos))+
+    geom_point()+
+    stat_smooth(mapping = aes(x = Dia,y = Casos,colour="Casos"))
